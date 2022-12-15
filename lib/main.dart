782d7d089dbeb96cb45ff24b631a5fe34a51717d
Diego <diego@furlcloud.com>
2022-12-15 11:08:20 +0000

    Added Yellow button to start/stop FX via soundpool

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flame_audio/audio_pool.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:soundpool/soundpool.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +35,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late Timer _stepsTimer;
   late Timer _poolStepsTimer;
+  late Timer _soundPoolStepsTimer;
 
   late AudioPool _stepPool;
+
+  late Soundpool _soundPool = Soundpool.fromOptions();
+  late int _soundId;
 
   bool _loaded = false;
 
@@ -50,6 +56,11 @@ class _MyHomePageState extends State<MyHomePage> {
     _stepPool = await FlameAudio.createPool('test-step.mp3',
         minPlayers: 4, maxPlayers: 4);
 
+    _soundPool = Soundpool.fromOptions();
+    _soundId = await rootBundle.load('assets/audio/test-step.mp3').then((ByteData soundData) {
+      return _soundPool.load(soundData);
+    });
+
     setState(() {
       _loaded = true;
     });
@@ -64,50 +75,77 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: _loaded
             ? Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            const Text(
+              'Start/stop FX and BGM by pressing Buttons below.',
+            ),
+            Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  const Text(
-                    'Push button to start audio',
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(children: [
-                          const Text('BGM Music'),
-                          ElevatedButton(
-                            onPressed: _startBgm,
-                            child: const Text('Start'),
-                          ),
-                          ElevatedButton(
-                            onPressed: _stopBgm,
-                            child: const Text('Stop'),
-                          )
-                        ]),
-                        Column(children: [
-                          const Text('Steps FX via FlameAudio.Play'),
-                          ElevatedButton(
-                            onPressed: _startSteps,
-                            child: const Text('Start'),
-                          ),
-                          ElevatedButton(
-                            onPressed: _stopSteps,
-                            child: const Text('Stop'),
-                          )
-                        ]),
-                        Column(children: [
-                          const Text('Steps FX via AudioPool.Start'),
-                          ElevatedButton(
-                            onPressed: _startPoolSteps,
-                            child: const Text('Start'),
-                          ),
-                          ElevatedButton(
-                            onPressed: _stopPoolSteps,
-                            child: const Text('Stop'),
-                          )
-                        ])
-                      ]),
-                ],
-              )
+                children: [
+                  Column(children: [
+                    const Text('Steps FX via FlameAudio.play()'),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue),
+                      onPressed: _startSteps,
+                      child: const Text('Start'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue),
+                      onPressed: _stopSteps,
+                      child: const Text('Stop'),
+                    )
+                  ]),
+                  Column(children: [
+                    const Text('Steps FX via AudioPool.start()'),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red),
+                      onPressed: _startPoolSteps,
+                      child: const Text('Start'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red),
+                      onPressed: _stopPoolSteps,
+                      child: const Text('Stop'),
+                    )
+                  ]),
+                  Column(children: [
+                    const Text('Steps FX via Soundpool.play()'),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow),
+                      onPressed: _startSoundPoolSteps,
+                      child: const Text('Start'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow),
+                      onPressed: _stopSoundPoolSteps,
+                      child: const Text('Stop'),
+                    )
+                  ]),
+                  Column(children: [
+                    const Text('BGM Long Tone (~1m 10s)'),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green),
+                      onPressed: _startBgm,
+                      child: const Text('Start'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green),
+                      onPressed: _stopBgm,
+                      child: const Text('Stop'),
+                    )
+                  ]),
+                ]),
+          ],
+        )
             : Container(),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
@@ -136,11 +174,22 @@ class _MyHomePageState extends State<MyHomePage> {
   _startPoolSteps() {
     _poolStepsTimer =
         Timer.periodic(const Duration(milliseconds: 250), (t) async {
-      await _stepPool.start(volume: 1.0);
-    });
+          await _stepPool.start(volume: 1.0);
+        });
   }
 
   _stopPoolSteps() {
     _poolStepsTimer.cancel();
+  }
+
+  _startSoundPoolSteps() {
+    _soundPoolStepsTimer =
+        Timer.periodic(const Duration(milliseconds: 250), (t) async {
+          await _soundPool.play(_soundId);
+        });
+  }
+
+  _stopSoundPoolSteps() {
+    _soundPoolStepsTimer.cancel();
   }
 }
